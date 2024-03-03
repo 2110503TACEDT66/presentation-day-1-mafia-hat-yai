3ed120519dfee48277f0d534bdf271804053cb00
Daniel Samae <danielsamae555@gmail.com>
2024-03-03 18:18:10 +0700

    server.js & routes

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 // Route files
-const hospitals = require('./routes/hospitals');
+const restaurants = require('./routes/restaurants');
 const auth = require('./routes/auth');
-const appointments = require('./routes/appointments');
+const reservations=require('./routes/reservations');
 
 const app = express()
 
@@ -52,9 +52,9 @@ app.use(mongoSanitize());
 app.use(cookieParser());
 
 // Mount routers
-app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/restaurants',restaurants);
 app.use('/api/v1/auth', auth);
-app.use('/api/v1/appointments', appointments);
+app.use('/api/v1/reservations', reservations);
 
 const PORT = process.env.PORT || 5000;
 
